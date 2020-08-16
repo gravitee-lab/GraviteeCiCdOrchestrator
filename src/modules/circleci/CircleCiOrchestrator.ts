c6b24b6bd06cbe257c363cdb8e7bf1719a4e2fa3
Jean-Baptiste-Lasselle <jean.baptiste.lasselle.pegasus@gmail.com>
2020-08-16 19:01:00 +0200

    feat(specs_implementation) : module [CircleCiOrchestrator.ts], implementing trigger Circle CI Pipeline Builds , working on #2

--- src/modules/circleci/CircleCiOrchestrator.ts
+++ src/modules/circleci/CircleCiOrchestrator.ts
@@ -45,8 +45,8 @@ export class CircleCiOrchestrator {
     processExecutionSet (parallelExecutionsSet: string[]) : void {
 
       parallelExecutionsSet.forEach((dependency, index) => {
-        let observableRequest = this.circleci_client.whoami();
-        let subscription = observableRequest.subscribe( {
+        /// let observableRequest = this.circleci_client.whoami();
+        let subscription = this.circleci_client.whoami().subscribe( {
             next: data => console.log( '[data] => ', data ),
             complete: data => console.log( '[complete]' )
         } );
@@ -60,8 +60,8 @@ export class CircleCiOrchestrator {
 
 /**
  *
- * Mimics the official CircleCI cLient, only much simpler, and with [RxJS]
- *
+ * Mimics the official Circle CI cLient, only much simpler, and with [RxJS]
+ * Circle CI API v2 based
  **/
 export class CircleCIClient {
   private secrets: any;
@@ -82,15 +82,102 @@ export class CircleCIClient {
   }
 
     /**
-     * Triggers a Circle CI Pipeline
+     * Triggers a Circle CI Pipeline, for a repo on Github
+     *
+     * -----
+     * <pre>
+     *      version: 2.1
+     *      jobs:
+     *        build:
+     *          docker:
+     *            - image: "circleci/node:<< pipeline.parameters.image-tag >>"
+     *          environment:
+     *            IMAGETAG: "<< pipeline.parameters.image-tag >>"
+     *          steps:
+     *            - run: echo "Image tag used was ${IMAGETAG}"
+     *      parameters:
+     *        image-tag:
+     *          default: latest
+     *          type: string
+     * </pre>
+     * -----
+     *
+     *
      *
      * @argument username {@type string} the Circle CI username , eg "jpstevens",
-     * @argument project {@type string} the Circle CI project name, matching the github repo name, e.g."circleci",
+     * @argument org_name  {@type string} the github organization name if the git repo in on github.com
+     * @argument repo_name {@type string} the Circle CI project name, matching the github repo name, e.g."circleci",
      * @argument branch {@type string} the git branch "master" on which to trigger the pipeline
+     * @argument pipelineParameters {@type any} the Circle CI pipeline parameters. For Example, a  :
+     *
+     * -----
+     * <pre>
+     *      version: 2.1
+     *      jobs:
+     *        build:
+     *          docker:
+     *            - image: "circleci/node:<< pipeline.parameters.image-tag >>"
+     *          environment:
+     *            IMAGETAG: "<< pipeline.parameters.image-tag >>"
+     *            GRAVITEEIO_VERSION: "<< pipeline.parameters.graviteeio-version >>"
+     *            GRAVITEEIO_VERSION_MINOR: "<< pipeline.parameters.graviteeio-version-minor >>"
+     *            GRAVITEEIO_VERSION_MAJOR: "<< pipeline.parameters.graviteeio-version-major >>"
+     *          steps:
+     *            - run: echo "Docker Image tag used was ${IMAGETAG}"
+     *            - run: echo "GRAVITEEIO_VERSION used was ${GRAVITEEIO_VERSION}"
+     *            - run: echo "GRAVITEEIO_VERSION_MINOR used was ${GRAVITEEIO_VERSION_MINOR}"
+     *            - run: echo "GRAVITEEIO_VERSION_MAJOR used was ${GRAVITEEIO_VERSION_MAJOR}"
+     *      parameters:
+     *        image-tag:
+     *          default: latest
+     *          type: string
+     *        graviteeio-version:
+     *          default: 3.1.0
+     *          type: string
+     *        graviteeio-version-minor:
+     *          default: 3.1
+     *          type: string
+     *        graviteeio-version-major:
+     *          default: 3
+     *          type: string
+     *
+     * </pre>
+     * -----
+     *
+     *  then <pre>pipelineParameters</pre> coudl for example be :
+     *
+     *
+     * -----
+     * <pre>
+     * {
+     *    "parameters": {
+     *      "image-tag": "3.1.14-full",
+     *      "graviteeio-version": "3.1.14",
+     *      "graviteeio-version-minor": "3.1",
+     *      "graviteeio-version-major": "3"
+     *    }
+     *  }
+     * </pre>
+     * -----
+     *
+     *
+     * @returns any But it actually is an Observable Stream of the HTTP response you can subscribe to.
      **/
-    triggerBuild(username: string, project: string, branch: string): Observable<any>{
+    triggerGhBuild(username: string, org_name: string, repo_name: string, branch: string, pipelineParameters: string): any {
       let observableRequest = Observable.create( ( observer ) => {
-          axios.get( 'https://circleci.com/api/v2/me' )
+          let config = {
+            headers: {
+              "Circle-Token": this.secrets.circleci.auth.token,
+              "Accept": "application/json",
+              "Content-Type": "application/json"
+            }
+          };
+
+          let jsonPayloadExample = {
+            'branch': `${branch}`
+          };
+          /// axios.post( 'https://circleci.com/api/v2/me', jsonPayloadExample, config ).then(....)
+          axios.post( "https://circleci.com/api/v2/gh/" + `${org_name}` + "/" + `${repo_name}` + "/pipeline", jsonPayloadExample, config )
           .then( ( response ) => {
               observer.next( response.data );
               observer.complete();
@@ -99,6 +186,7 @@ export class CircleCIClient {
               observer.error( error );
           } );
       } );
+
       return observableRequest;
     }
     whoami(): any {
@@ -106,13 +194,9 @@ export class CircleCIClient {
           let config = {
             headers: {
               "Circle-Token": this.secrets.circleci.auth.token,
+              "Accept": "application/json"
             }
           };
-
-          let jsonPayloadExample = {
-            'if I have to': 'ccc'
-          };
-          /// axios.post( 'https://circleci.com/api/v2/me', jsonPayloadExample, config ).then(....)
           axios.get( 'https://circleci.com/api/v2/me', config )
           .then( ( response ) => {
               observer.next( response.data );
@@ -122,10 +206,7 @@ export class CircleCIClient {
               observer.error( error );
           } );
       } );
-      let subscription = observableRequest.subscribe( {
-          next: data => console.log( '[data] => ', data ),
-          complete: data => console.log( '[complete]' ),
-      } );
+
       return observableRequest;
     }
 }
