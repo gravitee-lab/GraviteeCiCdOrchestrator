743c1e92d1ddf728ec460afe981a598429426d58
Jean-Baptiste-Lasselle <jean.baptiste.lasselle.pegasus@gmail.com>
2020-09-12 20:33:28 +0200

    feat(specs_implementation) : replacing progress Matrix with Observable Stream pattern everywhere #22

--- src/modules/monitor/Monitor.ts
+++ src/modules/monitor/Monitor.ts
@@ -137,111 +137,15 @@ export namespace monitoring {
     }
 
     private initSubscribers() : void {
-      let arrayLength = this.parallelExecutionSetProgress.pipeline_executions.length;
+      let arrayLength = this.parallelExecutionSetProgress.all_pipeline_execution_progress.length;
       for (let i: number; i < arrayLength ; i++){
         // create a new Subscriber which immediately subscribes to PipelineExecution 's observableRequest
-        let thisSubscriber = new monitoring.subscribers.CciApiTriggerPipelineSubscriber(this.parallelExecutionSetProgress.pipeline_executions[i]);
+        let thisSubscriber = new monitoring.subscribers.CciApiTriggerPipelineSubscriber(this.parallelExecutionSetProgress.all_pipeline_execution_progress[i]);
         // keep a reference over the new subscriber
         this.triggerSubscribers.push(thisSubscriber);
       }
     }
-    /**
-     * This method is there to serve as handler method for the <strong>Circle CI </strong> API call that trigger <strong>Circle CI <strong> Pipeline :
-     * Every time this method is invoked, it adds an entry  in the {@see this.progressMatrix}, from the <pre>data</pre> returned by the <strong>Circle CI</strong> API call
-     * then, the {@see this.progressMatrix}
-     *
-     *
-     * @argument data A JSOn Object returned by the Circle CI API as Response of a Pipeline trigger
-     * -----
-     * <pre>
-     * {
-     *
-     *    "number": "17",
-     *    "id": "c08fe570-a3ea-4232-9ed8-432ed65921a1",
-     *    "state": "pending",
-     *    "created_at": "2020-08-16T18:18:01.891Z"
-     *
-     *  }
-     * </pre>
-     * -----
-     *
-     *
-     **/
-    public fetch (): rxjs.Observable<AxiosResponse<any>> {
-            const demo_rest_endpoint: string = "https://auth-nightly.gravitee.io/management/organizations/DEFAULT/environments/DEFAULT/domains/dine"
-            //emit fetch result every 1s
-            //emit value every 1s
-            //emit value every 1s
-            const source = rxjs.from(axios.get(`${demo_rest_endpoint}`)).pipe(
-            tap(val => console.log(`fetching ${demo_rest_endpoint} which you won't see `)),)
-            const response$ = source.pipe(
-              map(axiosResponse => {
-                if (!(axiosResponse.status == 200 || axiosResponse.status == 201 || axiosResponse.status == 203)) {
-                  //error will be picked up by retryWhen
-                  throw axiosResponse;
-                }
-                return axiosResponse; /// return value  HTTP Response Code si 200
-              }),
-              retryWhen(errors =>
-                errors.pipe(
-                  //log error message
-                  tap(axiosResponse => {
-                    console.log(`Error occured, trying to fetch [${demo_rest_endpoint}], HTTP Response is : `);
-                    console.log(`Error occured, trying to fetch [${JSON.stringify(axiosResponse.data)}], now retrying`);
-                    console.log(`Error occured, trying to fetch [${demo_rest_endpoint}], now retrying`);
 
-                  }),
-                  //restart in 5 seconds
-                  delay(2000), /// wait 2 seconds before retrying
-                  /// delayWhen(val => timer(val * 1000)),
-                  /// delayWhen(val => rxjs.timer(7 * 1000)), /// wait 7 seconds before retrying
-                  take(5)
-                )
-              )
-            );
-
-            return response$;
-
-      }
-    // perfect test is :
-    // curl -X DELETE https://auth-nightly.gravitee.io/management/organizations/DEFAULT/environments/DEFAULT/domains/dine
-    // {"message":"No JWT token found","http_status":401}
-    public demoRetryWhen (): rxjs.Observable<AxiosResponse<any>> {
-            const demo_rest_endpoint: string = "https://auth-nightly.gravitee.io/management/organizations/DEFAULT/environments/DEFAULT/domains/dine"
-            const source = rxjs.from(axios.delete(`${demo_rest_endpoint}`)).pipe(
-            tap(val => console.log(`fetching ${demo_rest_endpoint} which you won't see `)))
-            const response$ = source.pipe(
-              map(axiosResponse => {
-                if (!(axiosResponse.status == 200 || axiosResponse.status == 201 || axiosResponse.status == 203)) {
-                  //error will be picked up by retryWhen
-                  console.log(` Fetch Response' request is : [${axiosResponse.request}], `);
-                  console.log(` HTTP status is : [${axiosResponse.statusText}], `);
-                  console.log(` AxiosResponse config is [${axiosResponse.config}]`);
-
-                  throw axiosResponse;
-                }
-                return axiosResponse; /// return value  HTTP Response Code si 200
-              }),
-              retryWhen(errors =>
-                errors.pipe(
-                  //log error message
-                  tap(axiosResponse => {
-                    console.log(`What is passed on to tap >>  [${JSON.stringify(axiosResponse,null,2)}],`);
-                    console.log(`now retrying`);
-
-                  }),
-                  //restart in 5 seconds
-                  delay(2000), /// wait 2 seconds before retrying
-                  /// delayWhen(val => timer(val * 1000)),
-                  /// delayWhen(val => rxjs.timer(7 * 1000)), /// wait 7 seconds before retrying
-                  take(5)
-                )
-              )
-            );
-
-            return response$;
-
-      }
   }
 
 }
