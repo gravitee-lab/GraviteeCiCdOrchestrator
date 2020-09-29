95d39a5c80eab5d3bc173bbb49ba542fd53f0430
Jean-Baptiste-Lasselle <jean.baptiste.lasselle.pegasus@gmail.com>
2020-09-29 18:36:50 +0200

    feat(specs_implementation) : writing a script to automate setup of circleci config on all required repos https://github.com/gravitee-io/release/issues/132

--- src/modules/circleci/status/tests/setup-test-repos.sh
+++ src/modules/circleci/status/tests/setup-test-repos.sh
@@ -2,7 +2,7 @@
 
 Usage () {
   echo "---"
-  echo " The [$0] script will commit and push the [.circleci/config.yml] on every [*.*.x] git branch of every git repo    "  
+  echo " The [$0] script will commit and push the [.circleci/config.yml] on every [*.*.x] git branch of every git repo    "
   echo "---"
   echo "Usage :"
   echo "---"
@@ -19,8 +19,22 @@ if [ "x${REPOS_URL_LIST_FILE}" == "x" ]; then
 fi;
 
 
-
-echo "---"
 echo "---"
 echo "  REPOS_URL_LIST_FILE=[${REPOS_URL_LIST_FILE}]"
 echo "---"
+
+# ------
+# -- FUNCTIONS
+# ------
+
+setupCircleCIConfig () {
+  export THIS_REPO_URL=$1
+  echo"[setupCircleCIConfig => ] processing [${THIS_REPO_URL}]"
+}
+# ------
+# -- OPS
+# ------
+while read REPO_URL; do
+  echo "---"
+  setupCircleCIConfig ${REPO_URL}
+done <${REPOS_URL_LIST_FILE}
