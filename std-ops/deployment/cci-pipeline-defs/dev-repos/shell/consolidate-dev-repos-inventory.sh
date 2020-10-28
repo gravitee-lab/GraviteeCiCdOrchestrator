249a008264a2359b3e8483d6ee7684f79cc138f7
Jean-Baptiste-Lasselle <jean.baptiste.lasselle.pegasus@gmail.com>
2020-10-28 17:36:07 +0100

    feat.(pipelines_autodeploy): automation of pipelines defintion, adding optimization avoiding repository redundancy,  working on https://github.com/gravitee-io/release/issues/155

--- std-ops/deployment/cci-pipeline-defs/dev-repos/shell/consolidate-dev-repos-inventory.sh
+++ std-ops/deployment/cci-pipeline-defs/dev-repos/shell/consolidate-dev-repos-inventory.sh
@@ -103,7 +103,97 @@ echo "rm -fr ${SCOPE_FILES_DIR}"
 echo "$0"
 echo "--->> <<---"
 
+if [ -f ${SCOPE_FILES_DIR}/scope-files.list ]; then
+  rm -f ${SCOPE_FILES_DIR}/scope-files.list
+fi;
+
+if [ -d ${OPS_DIR}/gitops ]; then
+  rm -fr ${OPS_DIR}/gitops
+fi;
+
+# rm -f ${OPS_DIR}/*.sh
+# rm -fr ${OPS_DIR}/.circleci
+
+# cp ${SCOPE_FILES_DIR}/src/modules/circleci/status/tests/backup-repos.sh ${OPS_DIR}
+# cp ${SCOPE_FILES_DIR}/src/modules/circleci/status/tests/deploy-repo-pipeline-def.sh ${OPS_DIR}
+
+# cp -fR ${SCOPE_FILES_DIR}/src/modules/circleci/status/tests/.circleci ${OPS_DIR}
+
+# As required, we forget about [1.20.*] [1.25.*] [1.29.*]
+# echo "${SCOPE_FILES_DIR}/repos-scope.1.20.x.list" >> ${SCOPE_FILES_DIR}/scope-files.list
+# echo "${SCOPE_FILES_DIR}/repos-scope.1.25.x.list" >> ${SCOPE_FILES_DIR}/scope-files.list
+# echo "${SCOPE_FILES_DIR}/repos-scope.1.29.x.list" >> ${SCOPE_FILES_DIR}/scope-files.list
+# echo "${SCOPE_FILES_DIR}/repos-scope.1.30.x.list" >> ${SCOPE_FILES_DIR}/scope-files.list
+# echo "${SCOPE_FILES_DIR}/repos-scope.3.0.x.list" >> ${SCOPE_FILES_DIR}/scope-files.list
+# echo "${SCOPE_FILES_DIR}/repos-scope.3.1.x.list" >> ${SCOPE_FILES_DIR}/scope-files.list
+
+# --- #
+# As required, we forget about [1.20.*] [1.25.*] [1.29.*]
+ls ${SCOPE_FILES_DIR}/repos-scope.*.list | grep -v '1.20.x' | grep -v '1.25.x' | grep -v '1.29.x' | awk '{for (i=0; i < NF ;i++){print $i}}' | tee ${SCOPE_FILES_DIR}/scope-files.list
+
+
+# tree
+ls -allh .
+
+# cat scope-files.list
+cat ${SCOPE_FILES_DIR}/scope-files.list
+
+# --- #
+# --- #
+# each file listed in [scope-files.list] contain a list of URIs of git repositories.
+# two different files listed in [scope-files.list] may contain the same URIs of a git repository
+# --- #
+# Now merging all files listed in [scope-files.list], into
+# one unique file listing all URIs of git
+# repositories, without redundancy : [${OPS_DIR}/all-git-uris.list]
+
+# cat $(cat ${SCOPE_FILES_DIR}/scope-files.list | head -n 1) |  ${OPS_DIR}/all-git-uris.list
+
+ITERATION_COUNT=0
+while read FILEPATH; do
+  # echo "ITERATION NUMBER = [${ITERATION_COUNT}]"
+  if [ "x${ITERATION_COUNT}" == "x0" ]; then
+    # echo "This is the first iteration, of ITERATION NUMBER = [${ITERATION_COUNT}]"
+    # I begin by adding all git URIs from the first file
+    cat ${FILEPATH} | tee ${OPS_DIR}/all-git-uris.list
+  else
+    # echo "This is NOT the first iteration, and ITERATION NUMBER = [${ITERATION_COUNT}]"
+    while read CURRENT_GIT_URI; do
+      # is [CURRENT_GIT_URI] already in [${OPS_DIR}/all-git-uris.list] ?
+      export SEARCH_RESULT=$(cat ${OPS_DIR}/all-git-uris.list | grep ${CURRENT_GIT_URI})
+      if [ "x${SEARCH_RESULT}" == "x" ]; then
+        # if git URIs was not found in [${OPS_DIR}/all-git-uris.list], then we add it
+        echo "${CURRENT_GIT_URI}" | tee -a ${OPS_DIR}/all-git-uris.list
+      else
+        echo "EXCLUDED GT URI - The [${CURRENT_GIT_URI}] Git URI is already inside the [${OPS_DIR}/all-git-uris.list] - EXCLUDED "
+      fi;
+    done <${FILEPATH}
+  fi;
+  (( ITERATION_COUNT++ ))
+done <${SCOPE_FILES_DIR}/scope-files.list
+
+# And now, [${OPS_DIR}/all-git-uris.list] contains only
+# one occurrence of any GIT URI listed in Any of the [${SCOPE_FILES_DIR}/repos-scope.*.list] files
+
+# --- #
+# --- #
+echo "# --------------------------------------------------------------------------------------- #"
+echo "# --------------------------------------------------------------------------------------- #"
+echo "# --------------------------------------------------------------------------------------- #"
+echo "# ------------------ The [${OPS_DIR}/all-git-uris.list] file now contains  -------------- #"
+echo "# ------------------ the list of URIs of all the git repo to  ----------------------------#"
+echo "# ------------------ deploy the Circle CI Pipeline Definition to.  -----------------------#"
+echo "# --------------------------------------------------------------------------------------- #"
+echo "# --------------------------------------------------------------------------------------- #"
+echo "# --------------------------------------------------------------------------------------- #"
 exit 0
+
+
+# ---------------------------------------------------------------- #
+# ---------------------------------------------------------------- #
+# ---- KEPT BELOW : SOME UNIT TESTS USED TO DESIGN HIS SCRIPT ---- #
+# ---------------------------------------------------------------- #
+# ---------------------------------------------------------------- #
 
 echo "--->> <<---"
 echo "--->> testing function [generateScopeFile] : "
