de8dce84e3aa589c6f66c858af1c5e47e1bb1f22
Jean-Baptiste-Lasselle <jean.baptiste.lasselle.pegasus@gmail.com>
2020-12-15 11:58:20 +0100

    improving error logs messages

--- src/modules/manifest/ReleaseManifestFilter.ts
+++ src/modules/manifest/ReleaseManifestFilter.ts
@@ -264,7 +264,7 @@ export class ReleaseManifestFilter {
       /// Index must not be out of bounds of the [parallelizationConstraintsMatrix]
       /// Index must not be negative
       if (parallelExecutionSetIndexToReturn < 0) {
-        let errMsg = "{[ReleaseManifestFilter]} - Gravitee Release Orchestrator could not determine which Parallel Execution Set Index for the following component : ";
+        let errMsg = "{[ReleaseManifestFilter]} - Gravitee Release Orchestrator could not determine which Parallel Execution Set Index for the following component (do they appear in the [buildDependencies] in the [release.json] ? ) : ";
         errMsg += `${JSON.stringify(component, null, "  ")}`;
         errMsg += " ";
         throw new Error(errMsg)
