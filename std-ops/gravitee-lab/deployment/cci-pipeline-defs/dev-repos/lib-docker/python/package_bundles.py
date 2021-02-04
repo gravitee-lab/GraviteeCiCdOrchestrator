9483c52dcd0286e865199d7d67bd561350077bfb
Jean-Baptiste-Lasselle <jean-baptiste.lasselle@graviteesource.com>
2021-02-04 12:47:27 +0100

    feat.(zip-bundle-server): jenkins python script customization for artifactory auth

--- std-ops/gravitee-lab/deployment/cci-pipeline-defs/dev-repos/lib-docker/python/package_bundles.py
+++ std-ops/gravitee-lab/deployment/cci-pipeline-defs/dev-repos/lib-docker/python/package_bundles.py
@@ -230,7 +230,7 @@ def download(name, filename_path, url):
         # with urlopen(url) as in_stream, open(filename_path, 'wb') as out_file:
             # copyfileobj(in_stream, out_file)
         not os.path.exists(target_folder_path) or os.makedirs(target_folder_path)
-        with urlopen(url) as in_stream, open(filename_path, 'wb') as out_file:
+        with urlopen(url) as in_stream, open(filename_path, 'wb', -1) as out_file:
             shutil.copyfileobj(in_stream, out_file)
         # urlretrieve(url, filename_path) # original http call from Jenkins
         # # TODO JBL : add HTTP Basic Auth authentication https://stackoverflow.com/questions/44239822/urllib-request-urlopenurl-with-authentication
