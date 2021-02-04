23e5a3f5af0692d3a063f510162a910faa53a91e
Jean-Baptiste-Lasselle <jean-baptiste.lasselle@graviteesource.com>
2021-02-04 13:46:24 +0100

    feat.(zip-bundle-server): jenkins python script customization for artifactory auth

--- std-ops/gravitee-lab/deployment/cci-pipeline-defs/dev-repos/lib-docker/python/start.sh
+++ std-ops/gravitee-lab/deployment/cci-pipeline-defs/dev-repos/lib-docker/python/start.sh
@@ -8,4 +8,19 @@ touch /usr/src/gio_files/tmp/3.4.3/portals/gravitee-portal-webui-3.4.3.zip
 # export PATH="$PATH:/usr/src/app:/usr/src/app/tmp/${RELEASE_VERSION}/portals/"
 export PATH="$PATH:$FOLDER_FOR_ALL_DOWNLOADED_FILES:$FOLDER_FOR_ALL_DOWNLOADED_FILES/tmp/${RELEASE_VERSION}/portals/"
 
+echo "# ------------------------------------------------------------ #"
+echo "Juste avant de démarrer le script python : "
+echo "# ------------------------------------------------------------ #"
+echo "  PWD = [${PWD}]"
+echo "# ------------------------------------------------------------ #"
+echo "  existence du répertoire [/usr/src/gio_files/tmp/3.4.3/portals/] : "
+ls -allh /usr/src/gio_files/tmp/3.4.3/portals/
+echo "# ------------------------------------------------------------ #"
+echo "  existence du fichier [/usr/src/gio_files/tmp/3.4.3/portals/gravitee-portal-webui-3.4.3.zip] : "
+ls -allh /usr/src/gio_files/tmp/3.4.3/portals/gravitee-portal-webui-3.4.3.zip
+echo "# ------------------------------------------------------------ #"
+echo "   identité du user linux : "
+id
+echo "# ------------------------------------------------------------ #"
+
 python ./package_bundles.py
