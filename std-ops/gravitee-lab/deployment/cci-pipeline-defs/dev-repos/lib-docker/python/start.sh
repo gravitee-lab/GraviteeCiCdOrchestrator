064e02bdedb75e129389cf1e3dc722000f6fefc4
Jean-Baptiste-Lasselle <jean-baptiste.lasselle@graviteesource.com>
2021-02-04 13:48:34 +0100

    feat.(zip-bundle-server): jenkins python script customization for artifactory auth

--- std-ops/gravitee-lab/deployment/cci-pipeline-defs/dev-repos/lib-docker/python/start.sh
+++ std-ops/gravitee-lab/deployment/cci-pipeline-defs/dev-repos/lib-docker/python/start.sh
@@ -5,6 +5,7 @@ export FOLDER_FOR_ALL_DOWNLOADED_FILES=/usr/src/gio_files
 mkdir -p /usr/src/gio_files/tmp/3.4.3/portals/
 chmod a+rw /usr/src/gio_files/tmp/3.4.3/portals/
 touch /usr/src/gio_files/tmp/3.4.3/portals/gravitee-portal-webui-3.4.3.zip
+chmod a+rw /usr/src/gio_files/tmp/3.4.3/portals/gravitee-portal-webui-3.4.3.zip
 
 # export PATH="$PATH:/usr/src/app:/usr/src/app/tmp/${RELEASE_VERSION}/portals/"
 export PATH="$PATH:$FOLDER_FOR_ALL_DOWNLOADED_FILES:$FOLDER_FOR_ALL_DOWNLOADED_FILES/tmp/${RELEASE_VERSION}/portals/"
