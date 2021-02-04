3d69297959e528075b99bc1d1b17902b385c982c
Jean-Baptiste-Lasselle <jean-baptiste.lasselle@graviteesource.com>
2021-02-04 13:43:05 +0100

    feat.(zip-bundle-server): jenkins python script customization for artifactory auth

--- std-ops/gravitee-lab/deployment/cci-pipeline-defs/dev-repos/lib-docker/python/start.sh
+++ std-ops/gravitee-lab/deployment/cci-pipeline-defs/dev-repos/lib-docker/python/start.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-export INSTALL_OPS_HOME=$(mktemp -d -t "s3cmd_install_ops-XXXXXXXXXX")
+
 export FOLDER_FOR_ALL_DOWNLOADED_FILES=/usr/src/gio_files
-# export PATH="$PATH:/usr/src/app:/usr/src/app/tmp/${RELEASE_VERSION}/portals/"
-export PATH="$PATH:$FOLDER_FOR_ALL_DOWNLOADED_FILES:$FOLDER_FOR_ALL_DOWNLOADED_FILES/tmp/${RELEASE_VERSION}/portals/"
 
 mkdir -p /usr/src/gio_files/tmp/3.4.3/portals/
 touch /usr/src/gio_files/tmp/3.4.3/portals/gravitee-portal-webui-3.4.3.zip
+
+# export PATH="$PATH:/usr/src/app:/usr/src/app/tmp/${RELEASE_VERSION}/portals/"
+export PATH="$PATH:$FOLDER_FOR_ALL_DOWNLOADED_FILES:$FOLDER_FOR_ALL_DOWNLOADED_FILES/tmp/${RELEASE_VERSION}/portals/"
 
 python ./package_bundles.py
