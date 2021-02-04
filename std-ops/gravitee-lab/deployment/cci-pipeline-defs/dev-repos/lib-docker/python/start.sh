c47c24584044c21533298b012a3375a649ee15c8
Jean-Baptiste-Lasselle <jean-baptiste.lasselle@graviteesource.com>
2021-02-04 17:08:35 +0100

    feat.(zip-bundle-server): jenkins python script customization for artifactory auth

--- std-ops/gravitee-lab/deployment/cci-pipeline-defs/dev-repos/lib-docker/python/start.sh
+++ std-ops/gravitee-lab/deployment/cci-pipeline-defs/dev-repos/lib-docker/python/start.sh
@@ -2,26 +2,26 @@
 
 export FOLDER_FOR_ALL_DOWNLOADED_FILES=/usr/src/gio_files
 
-mkdir -p /usr/src/gio_files/tmp/3.4.3/portals/
-chmod a+rw /usr/src/gio_files/tmp/3.4.3/portals/
+# mkdir -p /usr/src/gio_files/tmp/3.4.3/portals/
+# chmod a+rw /usr/src/gio_files/tmp/3.4.3/portals/
 # touch /usr/src/gio_files/tmp/3.4.3/portals/gravitee-portal-webui-3.4.3.zip
 # chmod a+rw /usr/src/gio_files/tmp/3.4.3/portals/gravitee-portal-webui-3.4.3.zip
 
 # export PATH="$PATH:/usr/src/app:/usr/src/app/tmp/${RELEASE_VERSION}/portals/"
-export PATH="$PATH:$FOLDER_FOR_ALL_DOWNLOADED_FILES:$FOLDER_FOR_ALL_DOWNLOADED_FILES/tmp/${RELEASE_VERSION}/portals/:/usr/src/app"
+# export PATH="$PATH:$FOLDER_FOR_ALL_DOWNLOADED_FILES:$FOLDER_FOR_ALL_DOWNLOADED_FILES/tmp/${RELEASE_VERSION}/portals/:/usr/src/app"
 
 echo "# ------------------------------------------------------------ #"
-echo "Juste avant de démarrer le script python : "
+echo "   Just before python script start up : "
 echo "# ------------------------------------------------------------ #"
-echo "  PWD = [${PWD}]"
-echo "# ------------------------------------------------------------ #"
-echo "  existence du répertoire [/usr/src/gio_files/tmp/3.4.3/portals/] : "
-ls -allh /usr/src/gio_files/tmp/3.4.3/portals/
+# echo "  PWD = [${PWD}]"
+# echo "# ------------------------------------------------------------ #"
+# echo "  existence du répertoire [/usr/src/gio_files/tmp/3.4.3/portals/] : "
+# ls -allh /usr/src/gio_files/tmp/3.4.3/portals/
 echo "# ------------------------------------------------------------ #"
 # echo "  existence du fichier [/usr/src/gio_files/tmp/3.4.3/portals/gravitee-portal-webui-3.4.3.zip] : "
 # ls -allh /usr/src/gio_files/tmp/3.4.3/portals/gravitee-portal-webui-3.4.3.zip
 echo "# ------------------------------------------------------------ #"
-echo "   identité du user linux : "
+echo "   Linux user identity : "
 id
 echo "# ------------------------------------------------------------ #"
 echo "  Path to the Python Executable : "
